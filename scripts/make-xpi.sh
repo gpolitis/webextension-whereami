8c779f4718811fb13d33d338de7e31d40ac2fbcb
Georgios Politis <gp@superpointer.com>
2024-01-22 00:06:53 +0100

    Removes the make-xpi.sh script.

--- scripts/make-xpi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SCRIPT=`readlink -f "$0"`
-SCRIPTPATH=`dirname "$SCRIPT"`
-
-(cd "$SCRIPTPATH"/.. && zip -r -FS whereami.zip * -x ./.git/\* --exclude whereami.zip --exclude ./scripts/\*)
