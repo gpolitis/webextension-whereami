7be127a4e0663ace511f878283e47a0fab6fcb87
Georgios Politis <gp@superpointer.com>
2024-01-22 20:20:26 +0100

    Use square icons for the toolbar button.

--- scripts/make-xpi.sh
+++ scripts/make-xpi.sh
@@ -3,4 +3,4 @@
 SCRIPT=`readlink -f "$0"`
 SCRIPTPATH=`dirname "$SCRIPT"`
 VERSION=`cat $SCRIPTPATH/../manifest.json | jq -r .version`
-(cd "$SCRIPTPATH"/.. && zip -r -FS whereami-v$VERSION.xpi * -x ./.git/\* --exclude \*.xpi --exclude ./scripts/\*)
+(cd "$SCRIPTPATH"/.. && zip -r -FS whereami-v$VERSION.xpi * -x ./.git/\* --exclude \*.xpi --exclude ./scripts/\* -x '**/.DS_Store')
